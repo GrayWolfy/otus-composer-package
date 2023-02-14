774ed9a1e7fff0e0cea79dfb0c35722fc2476e07
GrayWolfy <sergeypopov23@gmail.com>
2023-02-14 23:15:18 +0300

    Added PinguinDrawer class

---
+++ src/PinguinDrawer.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Graywolfy\OtusComposerPackage;
+
+class PinguinDrawer
+{
+    public function draw()
+    {
+        echo "       _~_     <br>";
+        echo "      (o o)    <br>";
+        echo "     /  V  \\   <br>";
+        echo "    /(  _  )\\  <br>";
+        echo "      ^^ ^^    <br>";
+    }
+}
